37799dda2695fe734e79e6d6b6b5923e5b1aea4a
Bryan <haloday873@gmail.com>
2020-12-19 00:04:04 -0500

    can move multiple tiles

--- Dungeon-Game/src/Player.java
+++ Dungeon-Game/src/Player.java
@@ -9,44 +9,43 @@ public class Player {
         this.strength = strength;
         this.health = health;
     }
+
     public void setLocation(int x, int y) {
         this.x = x;
         this.y = y;
         return;
     }
 
-    public boolean move(String move, Level level,boolean doMove) {
+    public boolean move(String move, Level level, boolean doMove) {
         try {
-            Tile tempTile = null;
-            switch (move) {
-                case "w":
-                    tempTile = level.map[this.x - 1][this.y]; //swapping tiles to move
-                    level.map[this.x - 1][this.y] = level.map[this.x][this.y];
-                    level.map[this.x][this.y] = tempTile;
-                    this.setLocation(this.x - 1, this.y);
-                    break;
-                case "a":
-                    tempTile = level.map[this.x][this.y-1]; //swapping tiles to move
-                    level.map[this.x][this.y-1] = level.map[this.x][this.y];
-                    level.map[this.x][this.y] = tempTile;
-                    this.setLocation(this.x, this.y-1);
-                    break;
-                case "s":
-                    tempTile = level.map[this.x +1][this.y]; //swapping tiles to move
-                    level.map[this.x + 1][this.y] = level.map[this.x][this.y];
-                    level.map[this.x][this.y] = tempTile;
-                    this.setLocation(this.x + 1, this.y);
-                    break;
-                case "d":
-                    tempTile = level.map[this.x][this.y + 1]; //swapping tiles to move
-                    level.map[this.x][this.y+1] = level.map[this.x][this.y];
-                    level.map[this.x][this.y] = tempTile;
-                    this.setLocation(this.x, this.y+1);
-                    break;
-
+            String Direction = move.substring(0, 1);
+            int Count = 1;
+            if (move.length() > 1) {
+                Count = Integer.parseInt(move.substring(1, 2));
+            }
+            int movex = 0;
+            int movey = 0;
+            switch (Direction) {
+                case "w" -> movex = -1;
+                case "a" -> movey = -1;
+                case "s" -> movex = 1;
+                case "d" -> movey = 1;
+            }
+            for (int i = 1; i < Count; i++) {
+                if (!level.map[this.x + movex * i][this.y + movey * i].symbol.equals("-")) {
+                    System.out.println(level.map[this.x + movex * i][this.y + movey * i].symbol);
+                    return false;
+                }
+            }
+            if(doMove) {
+                Tile tempTile = level.map[this.x + movex * Count][this.y + movey * Count]; //swapping tiles to move
+                level.map[this.x + movex * Count][this.y + movey * Count] = level.map[this.x][this.y];
+                level.map[this.x][this.y] = tempTile;
+                this.setLocation(this.x + movex * Count, this.y + movey * Count);
             }
             return true;
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            System.out.println("Error in Move");
             return false;
 
         }
