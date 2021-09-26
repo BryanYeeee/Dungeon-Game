d09edd39b65eb0bddb90c4152e73b7ccd57407fd
Bryan <haloday873@gmail.com>
2021-09-26 16:02:18 -0400

    number 1

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ import java.util.Iterator;
 import javax.imageio.ImageIO;
 import javax.sound.sampled.*;
 
-public class Main {// A
+public class Main {// A 1
 
     public static Player person;
     public static Dood boss;
